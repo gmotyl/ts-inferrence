abdc184bb28dc3fca26740d39ee772d98719f40e
Grzegorz Motyl <grzegorz.motyl@stepstone.com>
2020-11-12 12:07:49 +0100

    step 4 : pattern matching

---
+++ 4_pattern_matching.ts
@@ -0,0 +1,24 @@
+{
+
+  const elephantExample = {
+    trunkSize: 10,
+    eyesColor: "red",
+  };
+  const pythonExample = {
+    length: 50,
+  };
+  const whaleExample = {
+    volume: 30,
+  };
+
+  // TODO: get static types from values by using the typeof keyword.
+  type Elephant =
+  type Python =
+  type Whale =
+  type Animal = Elephant | Python | Whale;
+
+  const animalWhale: Animal = {
+    volume: 3,
+  };
+
+}
---
+++ 5_pattern_matching_advanced.ts
@@ -0,0 +1,45 @@
+{
+  const elephantExample = {
+    trunkSize: 10,
+    eyesColor: "red",
+  };
+  const pythonExample = {
+    length: 50,
+  };
+  const whaleExample = {
+    volume: 30,
+  };
+
+  // TODO: add a type attribute for each of our animals to make a unique standardized way
+  //       of identifying an “instance” of the animal type and check the correctness of objects.
+  type Elephant = typeof elephantExample;
+  type Python = typeof pythonExample;
+  type Whale = typeof whaleExample;
+  type Animal = Elephant | Python | Whale;
+
+  const animalWhale: Animal = {
+    type: "Whale",
+    volume: 3,
+  };
+  const animalWhaleErr: Animal = {
+    length: 100, // expected error: Type '{ length: number; type: "Whale"; }' is not assignable to type 'Animal'.
+    // Object literal may only specify known properties, and 'length' does not exist in type 'Whale'.
+    type: "Whale",
+  };
+
+  const printAnimalAttrs = (animal: Animal) => {
+    // define custom business logic for each data type
+    switch (animal.type) {
+      case "Elephant":
+        console.log(animal.trunkSize);
+        console.log(animal.eyesColor);
+        break;
+      case "Python":
+        console.log(animal.size);
+        break;
+      case "Whale":
+        console.log(animal.volume);
+        break;
+    }
+  };
+}
