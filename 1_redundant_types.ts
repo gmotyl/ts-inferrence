bc6ff497ee1d7010d9fa1aa467372a217e66fe76
Grzegorz Motyl <grzegorz.motyl@stepstone.com>
2020-11-12 11:26:58 +0100

    redundant types

---
+++ 1_redundant_types.ts
@@ -0,0 +1,9 @@
+// TODO: apply Typescript “type inference”
+{
+  const main = (): number => {
+    const a: number = 3;
+    const b: number = 4;
+    const c: number = a + b;
+    return c;
+  };
+}
