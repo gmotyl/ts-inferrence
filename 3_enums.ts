89ad35c5b42ed1fca25c5baef4d559e8d0ec0be5
Grzegorz Motyl <grzegorz.motyl@stepstone.com>
2020-11-12 12:07:12 +0100

    step 3 solution

--- 3_enums.ts
+++ 3_enums.ts
@@ -2,15 +2,15 @@
   const user = {
     id: 3,
     name: "Foo",
-    // TODO: Help the compiler to correctly infer state string as the enum optional type
-    // valid states are "new", "pending" and "done"
-    state: "new",
+    // Solution: If Typescript is not able to 100% correctly infer your static types,
+    // you can help the compiler by defining a sub- value of an object with as syntax.
+    state: "new" as "new" | "pending" | "done",
   };
 
   type User = typeof user;
 
   const changeUserName = (useToEdit: User, newName: string) => {
     useToEdit.name = newName;
-    useToEdit.state = "pendingggggg"; // expected error: Type '"pendingggggg"' is not assignable to type '"null" | "pending" | "done"'
+    useToEdit.state = "pendingggggg"; // error: Type '"pendingggggg"' is not assignable to type '"null" | "pending" | "done"'
   };
 }
